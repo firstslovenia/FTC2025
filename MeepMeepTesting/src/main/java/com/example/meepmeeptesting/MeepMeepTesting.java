2d57c7c75a0f993fe3b53185906b3f0a5502ba58
Vid Furlan <nalrufdiv@gmail.com>
2024-11-07 22:32:30 +0100

    Better starting trajectories

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,23 +13,52 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(1000);
 
+        Pose2d blockDropPose2d = new Pose2d(55, 55, Math.toRadians(45));
+        Vector2d blockPickupVector2d = new Vector2d(36, 27);
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(39.4224324932042, 39.4224324932042, Math.toRadians(188.22825), Math.toRadians(188.22825), 12)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(20, 61.5, Math.toRadians(270)))
                         .splineToConstantHeading(new Vector2d(0, 36), Math.toRadians(270))
+                        .waitSeconds(3)
                         .addDisplacementMarker(() -> {
-                            System.out.println("Hanging block");
+                            System.out.println("Hung block");
                         })
-                        .lineToConstantHeading(new Vector2d(25, 36))
-                        .splineToConstantHeading(new Vector2d(40, 40), Math.toRadians(0))
-                        .lineToSplineHeading(new Pose2d(48.5, 40, Math.toRadians(270)))
-                        //.splineToSplineHeading(new Pose2d(48.5, 40, Math.toRadians(270)), Math.toRadians(0))
 
-                        //.lineToSplineHeading(new Pose2d(48.5, 41, Math.toRadians(270))) // Can be skipped but it looks like it could knock down a game element (CCA. +0.15s)
-                        .splineToSplineHeading(new Pose2d(55, 55, Math.toRadians(45)), Math.toRadians(45))
-                        //.splineToSplineHeading(new Pose2d(48.5, 55), Math.toRadians(90))
+                        .setReversed(true)
+                        .splineToConstantHeading(new Vector2d(6, 42), Math.toRadians(0))
+                        .lineToSplineHeading(new Pose2d(26, 42, Math.toRadians(0)))
+                        .splineToConstantHeading(blockPickupVector2d, Math.toRadians(270))
+                        .waitSeconds(2)
+                        .addDisplacementMarker(() -> {
+                            System.out.println("Pickup");
+                        })
 
+                        .lineToConstantHeading(new Vector2d(36, 30))
+                        .splineToSplineHeading(blockDropPose2d, Math.toRadians(45))
+                        .waitSeconds(3)
+                        .addDisplacementMarker(() -> {
+                            System.out.println("Added block to box");
+                        })
+
+                        .setReversed(true)
+                        .splineToSplineHeading(new Pose2d(45, 27, Math.toRadians(0)), Math.toRadians(270))
+                        .waitSeconds(2)
+                        .addDisplacementMarker(() -> {
+                            System.out.println("Pickup");
+                        })
+
+                        .lineToConstantHeading(new Vector2d(45, 30))
+                        .splineToSplineHeading(blockDropPose2d, Math.toRadians(45))
+                        .waitSeconds(3)
+                        .addDisplacementMarker(() -> {
+                            System.out.println("Added block to box");
+                        })
+
+                        .lineToSplineHeading(new Pose2d(55, 15, Math.toRadians(90)))
+                        .splineToConstantHeading(new Vector2d(61, 15), Math.toRadians(90))
+                        .lineToConstantHeading(new Vector2d(61, 55))
                         .build());
 
 

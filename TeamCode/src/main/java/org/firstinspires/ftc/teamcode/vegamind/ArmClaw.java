b1a07bd140b873f9ce342f5caed2d8600dbbe35d
kweezo <jakob.tadej.vrtacnik@students.vegova.si>
2024-11-16 12:02:02 +0100

    added claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vegamind/ArmClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vegamind/ArmClaw.java
@@ -4,12 +4,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class ArmClaw {
     private Servo servo;
+    Button button;
 
     public ArmClaw(Servo servo) {
         this.servo = servo;
     }
 
     public void run(Boolean input) {
+        button.update(input);
 
+        if (!button.get_state())
+            servo.setPosition(1);
+        else
+            servo.setPosition(0);
     }
 }
